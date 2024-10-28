48cdd355231a4e854c11397d09dffb23d2df15b3
Dhionas Ribeiro Magalhães <68797443+dhionasrm@users.noreply.github.com>
2024-10-28 16:54:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-const cacheName = 'meu-app-cache';
+const cacheName = 'dhionasrm-site-cache';
 const assets = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/script.js'
+  '/styles.css', // adicione os caminhos corretos dos seus arquivos CSS e JS
+  '/script.js',
+  'images/icon-72x72.png',
+  'images/icon-192x192.png'
+  // inclua todos os ativos que deseja armazenar em cache
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
